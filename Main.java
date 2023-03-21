f53cd34e80e8371af5bf7c2501c12bbbf8599ed3
lucascruzestudo <lucascruzestudo@gmail.com>
2023-03-21 12:40:21 -0300

    leitura de input e prevenção de erros

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String []args){
-        int teste = 10;
-        System.out.println(teste);
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int value = 0;
+        boolean valid = false;
+        do {
+            try {
+                System.out.println("Type an int value: ");
+                value = s.nextInt();
+                valid = true;
+                break;
+            } catch (Exception e) {
+                System.out.println("ERROR: The value inserted is not valid, provide an int value.");
+                valid = false;
+                s.next(); 
+            }
+        } while (valid == false);
+        s.close();
+        System.out.println("The value provided by the user was: " + value);
     }
 }
