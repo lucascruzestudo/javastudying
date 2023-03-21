b6663435296dfb84e2ba07e5273828466da53b8e
lucascruzestudo <lucascruzestudo@gmail.com>
2023-03-21 13:24:37 -0300

    melhorias de código

--- Main.java
+++ Main.java
@@ -1,23 +1,31 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int value = 0;
-        boolean valid = false;
+        boolean isValid = false;
         do {
             try {
                 System.out.println("Type an int value: ");
-                value = s.nextInt();
-                valid = true;
+                value = scanner.nextInt();
+                isValid = true;
                 break;
-            } catch (Exception e) {
-                System.out.println("ERROR: The value inserted is not valid, provide an int value.");
-                valid = false;
-                s.next(); 
+            } catch (InputMismatchException e) {
+                System.out.println("ERROR: The value inserted is not a valid integer. Please provide an integer value.");
+                isValid = false;
+                consumeInvalidInput(scanner);
             }
-        } while (valid == false);
-        s.close();
+        } while (isValid == false);
+        scanner.close();
         System.out.println("The value provided by the user was: " + value);
     }
+
+    private static void consumeInvalidInput(Scanner scanner){
+        if (scanner.hasNext()) {
+            scanner.next(); // if there is a value in the input buffer, consumes it so the input buffer becomes empty again
+        }
+    }
+
 }
